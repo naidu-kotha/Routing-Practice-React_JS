897417b0d6825014161be54abb3441b5060a6046
naidu-kotha <srinivasnaidu1605@gmail.com>
2022-11-18 11:30:50 +0000

    routing practice

---
+++ src/components/About/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+import './index.css'
+
+const About = () => (
+  <div className="container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      alt="about"
+      className="logo"
+    />
+    <h1 className="heading">About</h1>
+    <p className="details">I love to create! I am a front end web developer</p>
+  </div>
+)
+
+export default About
---
+++ src/components/Header/index.js
@@ -0,0 +1,35 @@
+// Write your JS code here
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const Header = () => (
+  <nav className="nav-header">
+    <div className="logo-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/wave-logo-img.png"
+        alt="wave"
+        className="wave-img"
+      />
+      <p className="title">Wave</p>
+    </div>
+    <ul className="menu-container">
+      <li>
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link className="nav-link" to="/about">
+          About
+        </Link>
+      </li>
+      <li>
+        <Link className="nav-link" to="/contact">
+          Contact
+        </Link>
+      </li>
+    </ul>
+  </nav>
+)
+
+export default Header
